c0c0a5e8557899b1f29b9c1053a3316c681aeb70
Sandy Cormie <sandycormie@hotmail.com>
2023-10-03 15:08:57 +0100

    wip

--- app/src/components/Home/CreateOperationsDialogue/CreateOperationsDialogue.tsx
+++ app/src/components/Home/CreateOperationsDialogue/CreateOperationsDialogue.tsx
@@ -27,15 +27,7 @@ export function CreateOperationsDialogue({
     <Dialog open={open} onClose={handleClose} fullWidth={true}>
       <DialogTitle>Create Operations</DialogTitle>
       <DialogContent>
-        <Stack spacing={3}>
-          <ClientSelector
-            value={client}
-            onChange={(value) => setClient(value)}
-          />
-          <ClientSelector
-            value={client}
-            onChange={(value) => setClient(value)}
-          />
+        <Stack spacing={3} paddingTop={1}>
           <ClientSelector
             value={client}
             onChange={(value) => setClient(value)}
@@ -50,9 +42,11 @@ export function CreateOperationsDialogue({
           />
         </Stack>
       </DialogContent>
-      <DialogActions>
+      <DialogActions sx={{ padding: 3 }}>
         <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Subscribe</Button>
+        <Button onClick={handleClose} variant="outlined">
+          Next
+        </Button>
       </DialogActions>
     </Dialog>
   );

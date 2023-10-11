cabf943c2dcfbd15935530b7165113192e9f5b5a
Sandy Cormie <sandycormie@hotmail.com>
2023-10-11 15:43:20 +0100

    about to revert from nested list to table of categories

--- app/src/components/Home/CreateOperationsDialogue/stages/SetCategoriesStage.tsx
+++ app/src/components/Home/CreateOperationsDialogue/stages/SetCategoriesStage.tsx
@@ -4,17 +4,64 @@ import {
   Checkbox,
   Chip,
   DialogContentText,
+  FormControlLabel,
   IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Paper,
   Stack,
   TextField,
+  Typography,
 } from "@mui/material";
 import CommentIcon from "@mui/icons-material/Comment";
 import { useState } from "react";
+import { Padding } from "@mui/icons-material";
+import { allCategories, Category } from "./categories";
+
+function CategoryRow({
+  category,
+  indent = 0,
+}: {
+  category: Category;
+  indent?: number;
+}) {
+  return (
+    <>
+      <ListItem
+        key={category.name}
+        disablePadding
+        sx={{ borderBottom: 1, borderColor: "divider" }}
+      >
+        <ListItemButton
+          // onClick={handleToggle(value)}
+          dense
+        >
+          <ListItemIcon
+            sx={{ minWidth: "initial", paddingLeft: indent ? indent * 3 : 0 }}
+          >
+            <Checkbox
+              edge="start"
+              // checked={checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+              // inputProps={{ 'aria-labelledby': labelId }}
+              //     // checked={checked[0] && checked[1]}
+              //     // indeterminate={checked[0] !== checked[1]}
+              //     // onChange={handleChange1}
+            />
+          </ListItemIcon>
+          <ListItemText id={category.name} primary={category.name} />
+        </ListItemButton>
+      </ListItem>
+      {category.children?.map((category) => (
+        <CategoryRow category={category} indent={indent ? indent + 1 : 1} />
+      ))}
+    </>
+  );
+}
 
 export function SetCategoriesStage({
   categories,
@@ -23,64 +70,50 @@ export function SetCategoriesStage({
   categories: string[];
   setCategories: (value: string[]) => void;
 }) {
-  const [checked, setChecked] = useState([0]);
+  const [checked, setChecked] = useState([true, false]);
 
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([event.target.checked, event.target.checked]);
   };
+
+  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([event.target.checked, checked[1]]);
+  };
+
+  const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([checked[0], event.target.checked]);
+  };
+
+  const children = (
+    <Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
+      <FormControlLabel
+        label={<Typography variant="body2">Child 1</Typography>}
+        control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+      />
+      <FormControlLabel
+        label={<Typography variant="body2">Child 2</Typography>}
+        control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+      />
+    </Box>
+  );
 
   return (
     <Stack spacing={3} paddingTop={3}>
       <DialogContentText>
         Enter one or more search terms for the new operations.
       </DialogContentText>
-
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "background.paper",
-          border: 1,
-        }}
-      >
-        {[
-          0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2,
-          3, 0, 1, 2, 3,
-        ].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
-
-          return (
-            <ListItem key={value} disablePadding>
-              <ListItemButton
-                role={undefined}
-                onClick={handleToggle(value)}
-                sx={{ borderBottom: 1, borderColor: "divider" }}
-                dense
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(value) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ "aria-labelledby": labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+      <Paper>
+        <Box sx={{ padding: 2, borderBottom: 1, borderColor: "divider" }}>
+          <Typography variant="body2" component="span" fontWeight={500}>
+            Category
+          </Typography>
+        </Box>
+        <List disablePadding>
+          {allCategories.map((category) => (
+            <CategoryRow category={category} />
+          ))}
+        </List>
+      </Paper>
     </Stack>
   );
 }
